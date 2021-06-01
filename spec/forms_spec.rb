de6f0c5904520c7ee1e580b92ee9c9a71993a379
FabricioCSales <sales.fabricioc@gmail.com>
2021-05-31 21:55:16 -0300

    Inclusao de usuario e senha invalidos

--- spec/forms_spec.rb
+++ spec/forms_spec.rb
@@ -15,4 +15,36 @@ describe 'Forms' do
 
         expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
     end 
+    
+    it 'Login com senha incorreta' do
+        visit 'https://training-wheels-protocol.herokuapp.com/login'
+
+        fill_in 'username', with: 'stark'
+        fill_in 'password', with: '1234'
+        
+        click_button 'Login'
+
+        expect(find('#flash-messages').visible?).to be true
+        #expect(find('#flash-messages').text).to eql ' Senha é invalida! '
+        expect(find('#flash-messages').text).to include 'Senha é invalida!'
+        #
+        expect(find('#flash-messages')).to have_content 'Senha é invalida'
+    end
+
+    it 'Usuário não cadastrado' do
+        visit 'https://training-wheels-protocol.herokuapp.com/login'
+
+        fill_in 'username', with: 'teste'
+        fill_in 'password', with: '1234'
+
+        click_button 'Login'
+
+        expect(find('#flash-messages').visible?).to be true
+
+        expect(find('#flash-messages').text).to include 'O usuário informado não está cadastrado!'
+
+        expect(find('#flash-messages')).to have_content 'O usuário informado não está cadastrado!'
+    end
+
+    
 end
